5832e11e14283873f61732790aadac0ffc20797e
ArefOleg <serd-075@mail.ru>
2025-01-24 15:19:23 +0300

    sortAsc, sortDesc, sort

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,37 +4,9 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param="asc") {    
-    let tempComparePosition = null;
-    let tempPosition = null;
-    let arrOut = new Array();
-    for(let i = 0; i < arr.length; i++){
-        arrOut.push(arr[i]);
-    }  
-    if (param === 'desc') {
-        for (let i = 0; i < arrOut.length; i++) {
-            for (let k = 0; k < arrOut.length; k++) {
-                tempPosition = arrOut[k].localeCompare(arrOut[i], ['ru', 'en'], { caseFirst: "upper" });
-                if (tempPosition === -1) {
-                    tempComparePosition = arrOut[i];
-                    arrOut[i] = arrOut[k];
-                    arrOut[k] = tempComparePosition;
-                }                
-
-            }
-        }
-    }
-    else {
-        for (let i = 0; i < arrOut.length; i++) {
-            for (let k = 0; k < arrOut.length; k++) {
-                tempPosition = arrOut[k].localeCompare(arrOut[i], ['ru', 'en'], { caseFirst: "upper" });
-                if (tempPosition === 1) {
-                    tempComparePosition = arrOut[i];
-                    arrOut[i] = arrOut[k];
-                    arrOut[k] = tempComparePosition;
-                }
-            }
-        }
-    }    
-    return arrOut;
+export function sortStrings(arr, param = "asc") {
+    const arrOut = [...arr];
+    const sortAsc = (a, b) => a.localeCompare(b, ['ru', 'en'], { caseFirst: "upper" })
+    const sortDesc = (a, b) => b.localeCompare(a, ['ru', 'en'], { caseFirst: "upper" })
+    return arrOut.sort(param === "asc" ? sortAsc : sortDesc);
 }
