f89aa8e248a65ce1f564a24142e2b782109fc608
ArefOleg <serd-075@mail.ru>
2025-01-29 11:18:55 +0300

    2,3,4 задача из 3

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,24 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+    let symbolMap = new Map();
+    let symbolContainer = '';
+    let globalKey = 0;
+    const addRecInMap = (symbol) =>{
+        symbolContainer = symbolContainer + symbol;
+        if(symbolContainer.length <= size) symbolMap.set(globalKey, symbolContainer);
+    } 
+    const createRecInMap = (symbol) =>{
+        symbolContainer = symbol;
+        globalKey = globalKey + 1;
+        if(symbol.length <= size) symbolMap.set(globalKey, symbolContainer);
+    }
+    for (let i = 0; i < string.length; i++) {
+        symbolContainer[0] === string[i] ? addRecInMap(string[i]) : createRecInMap(string[i]);        
+    }
+    let result = '';
+    for(let elem of symbolMap.values()){
+        result = result + elem;
+    }
+    return typeof size !== "undefined" ? result : string;
 }
