36d619a23c641b94d0b5cdc4b4dfd6b30382c0f6
ArefOleg <serd-075@mail.ru>
2025-02-02 20:08:41 +0300

    первый проброс, не в пул реквест

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,3 +1,36 @@
-export default class ColumnChart {
-
-}
+export default class ColumnChar {
+    constructor({ data, label, link, value } = {}) {
+      this.data = data || [];
+      this.label = label || '';
+      this.link = link || '';
+      this.value = value || 0;
+      this.element = this.createColumnCharElement();
+    }
+    createColumnCharElement() {
+      const element = document.createElement('div');
+      element.innerHTML = this.getStaticOrderTemplate();
+      return element.firstElementChild;
+    }
+    getStaticOrderTemplate() {
+      let val = ''
+      for (let elem of this.data) {
+        val = val + `
+          <div style="--value: ${elem}" data-tooltip="${elem * 2}%"></div>
+        `;
+      }
+  
+      return `<div class="dashboard__chart_orders">
+      <div class="column-chart" style="--chart-height: 50">
+        <div class="column-chart__title">
+          ${this.label}
+          <a href="/${this.link}" class="column-chart__link">View all</a>
+        </div>
+        <div class="column-chart__container">
+          <div data-element="header" class="column-chart__header">${this.value}</div>
+          <div data-element="body" class="column-chart__chart">
+            ${val}
+        </div>
+      </div>
+    </div>`
+    }
+  }
