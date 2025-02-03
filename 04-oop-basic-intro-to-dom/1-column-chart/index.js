9dba992e87b7096e5b42af6a0aa19564f5673d0b
ArefOleg <serd-075@mail.ru>
2025-02-03 11:06:21 +0300

    поправил еще баги

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -7,16 +7,30 @@ export default class ColumnChar {
     this.element = this.createColumnCharElement();
     this.chartHeight = 50;
     this.diagramParam = this.getColumnProps(this.data);
+    this.template = this.getTemplateName(this.label);
   }
+
   createColumnCharElement() {
     const element = document.createElement('div');
-    element.innerHTML = this.getStaticOrderTemplate();
+    element.innerHTML = this.getTemplate();
     return element.firstElementChild;
   }
+
+  getTemplateName(label){
+    switch(label){
+      case 'orders': return 'dashboard__chart_orders';
+      case 'sales': return 'dashboard__chart_sales';
+      case 'customers': return 'dashboard__chart_sales';
+      default: return 'empty';
+    }
+  }
+
   destroy() {
     this.element.remove();
   }
+
   getColumnProps(data) {
+    if (!data) return [];
     const maxValue = Math.max(...data);
     const scale = 50 / maxValue;
 
@@ -27,10 +41,12 @@ export default class ColumnChar {
       };
     });
   }
-  getStaticOrderTemplate() {
-    const diagramParamFromProp = this.diagramParam.map(({ percent, value }) => `
+
+  getTemplate() {
+    const diagramParamFromProp = (this.diagramParam || []).map(({ percent, value }) => `
       <div style="--value: ${value}" data-tooltip="${percent}%"></div>
     `).join('');
+
     return `<div class="${this.template}">
       <div class="column-chart" style="--chart-height: ${this.chartHeight}">
         <div class="column-chart__title">
