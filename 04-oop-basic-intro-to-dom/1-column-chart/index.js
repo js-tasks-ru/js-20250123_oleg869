60650262dbedbb402104f5b7a1051cd70a5b5016
ArefOleg <serd-075@mail.ru>
2025-02-04 08:13:38 +0300

    удалось сделать почти все проверки, кроме formatHeading

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,20 +1,25 @@
-export default class ColumnChar {
+export default class ColumnChart {
   constructor({ data, label, link, value, formatHeading } = {}) {
     this.data = data || [];
     this.label = label || '';
     this.link = link || '';
-    this.value = value || 0;
-    this.element = this.createColumnCharElement();
+    this.value = value || 0;    
     this.chartHeight = 50;
-    this.diagramParam = this.getColumnProps(this.data);
-    this.template = this.getTemplateName(this.label);
-    this.formatHeading = formatHeading;
+    this.formatHeading = formatHeading;    
+    this.element = this.createColumnChartElement();
+    this.elementFromDataBody = this.getDataElementsFromBody();
   }
 
-  createColumnCharElement() {
+  createColumnChartElement() {
     const element = document.createElement('div');
     element.innerHTML = this.getTemplate();
     return element.firstElementChild;
+  }
+
+  update(data){
+    this.data = data;
+    //this.getDataElementsFromBody.body.innerHTML = this.setColumnProps();//берем с урока обновление через innerHtml
+    this.elementFromDataBody.body.innerHTML = this.setColumnProps();
   }
 
   getTemplateName(label) {
@@ -26,7 +31,17 @@ export default class ColumnChar {
     }
   }
 
+  getDataElementsFromBody(){
+    //const elements = this.element.querySelectorAll('[data-element]'); // не массив, а prototype = nodelist
+    const element = this.element.querySelector('[data-element="body"]');
+    return {body : element}; // ключ + свойство , можно к нему будет обратиться при обновлении    
+  }
+
   destroy() {
+    this.remove();
+  }
+
+  remove(){
     this.element.remove();
   }
 
@@ -34,7 +49,7 @@ export default class ColumnChar {
     if (!data) return [];
     const maxValue = Math.max(...data);
     const scale = 50 / maxValue;
-
+    //console.log(data); //arr
     return data.map(item => {
       return {
         percent: (item / maxValue * 100).toFixed(0) + '%',
@@ -43,23 +58,40 @@ export default class ColumnChar {
     });
   }
 
-  getTemplate() {
-    const diagramParamFromProp = (this.diagramParam || []).map(({ percent, value }) => `
-      <div style="--value: ${value}" data-tooltip="${percent}%"></div>
+  setColumnProps(){
+    const diagramParamFromProp = (this.getColumnProps(this.data) || []).map(({ percent, value }) => `
+      <div style="--value: ${value}" data-tooltip="${percent}"></div>
     `).join('');
+    return diagramParamFromProp;
+  }
 
-    return `<div class="${this.template}">
-      <div class="column-chart" style="--chart-height: ${this.chartHeight}">
-        <div class="column-chart__title">
-          ${this.label}
-          <a href="/${this.link}" class="column-chart__link">View all</a>
-        </div>
-        <div class="column-chart__container">
-          <div data-element="header" class="column-chart__header">${this.value}</div>
-          <div data-element="body" class="column-chart__chart">
-          ${diagramParamFromProp}
-        </div>
-      </div>
-    </div>`
+  getTemplate() {    
+    return `
+            <div class="${this.getTemplateName(this.label)}">
+                <div class="column-chart" style="--chart-height: ${this.chartHeight}">
+                  <div class="column-chart__title">
+                     ${this.label}
+                    <a href="/${this.link}" class="column-chart__link">View all</a>
+                  </div>
+                  <div class="column-chart__container">
+                    <div data-element="header" class="column-chart__header">${this.value}</div>
+                    <div data-element="body" class="column-chart__chart">
+                      ${this.setColumnProps()}
+                    </div>
+                  </div>
+                </div>
+              </div>`;
   }
 }
+
+const columnChart = new ColumnChart({
+  data: [20, 20, 10], 
+  label: 'customers',           
+  link: 'sales',              
+  value: 2045   
+});
+
+
+document.body.append(columnChart.element);
+console.log(columnChart.getDataElementsFromBody());
+columnChart.update([10, 40, 73, 29, 9, 32]);
