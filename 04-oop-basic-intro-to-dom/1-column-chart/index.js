d0ef4ed4e0ed53cd0b0483d2c5fc81ecf88013bd
ArefOleg <serd-075@mail.ru>
2025-02-04 09:09:30 +0300

    переделал formatHeading как из урока

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,11 +1,16 @@
 export default class ColumnChart {
-  constructor({ data, label, link, value, formatHeading } = {}) {
+  constructor({ data,
+    label,
+    link,
+    value,
+    formatHeading = value => value
+  } = {}) {
     this.data = data || [];
     this.label = label || '';
     this.link = link || '';
-    this.value = value || 0;    
+    this.value = value || 0;
     this.chartHeight = 50;
-    this.formatHeading = formatHeading;    
+    this.formatHeading = formatHeading;
     this.element = this.createColumnChartElement();
     this.elementFromDataBody = this.getDataElementsFromBody();
   }
@@ -16,9 +21,8 @@ export default class ColumnChart {
     return element.firstElementChild;
   }
 
-  update(data){
+  update(data) {
     this.data = data;
-    //this.getDataElementsFromBody.body.innerHTML = this.setColumnProps();//берем с урока обновление через innerHtml
     this.elementFromDataBody.body.innerHTML = this.setColumnProps();
   }
 
@@ -31,17 +35,16 @@ export default class ColumnChart {
     }
   }
 
-  getDataElementsFromBody(){
-    //const elements = this.element.querySelectorAll('[data-element]'); // не массив, а prototype = nodelist
+  getDataElementsFromBody() {
     const element = this.element.querySelector('[data-element="body"]');
-    return {body : element}; // ключ + свойство , можно к нему будет обратиться при обновлении    
+    return {body : element};
   }
 
   destroy() {
     this.remove();
   }
 
-  remove(){
+  remove() {
     this.element.remove();
   }
 
@@ -49,7 +52,6 @@ export default class ColumnChart {
     if (!data) return [];
     const maxValue = Math.max(...data);
     const scale = 50 / maxValue;
-    //console.log(data); //arr
     return data.map(item => {
       return {
         percent: (item / maxValue * 100).toFixed(0) + '%',
@@ -58,39 +60,39 @@ export default class ColumnChart {
     });
   }
 
-  setColumnProps(){
+  setColumnProps() {
     const diagramParamFromProp = (this.getColumnProps(this.data) || []).map(({ percent, value }) => `
       <div style="--value: ${value}" data-tooltip="${percent}"></div>
     `).join('');
     return diagramParamFromProp;
   }
 
-  getTemplate() {    
+  getTemplate() {
     return `
             <div class="${this.getTemplateName(this.label)}">
                 <div class="column-chart" style="--chart-height: ${this.chartHeight}">
                   <div class="column-chart__title">
                      ${this.label}
-                    <a href="/${this.link}" class="column-chart__link">View all</a>
+                  <a href="/${this.link}" class="column-chart__link">View all</a>
                   </div>
                   <div class="column-chart__container">
                     <div data-element="header" class="column-chart__header">
-                      ${this.formatHeading ? this.formatHeading(this.value) : this.value}
+                      ${this.formatHeading(this.value)}
                     </div>
                     <div data-element="body" class="column-chart__chart">
                       ${this.setColumnProps()}
                     </div>
                   </div>
                 </div>
-              </div>`;
+            </div>`;
   }
 }
 
 const columnChart = new ColumnChart({
-  data: [20, 20, 10], 
-  label: 'customers',           
-  link: 'sales',              
-  value: 2045   
+  data: [20, 20, 10],
+  label: 'customers',
+  link: 'sales',
+  value: 2045
 });
 
 
