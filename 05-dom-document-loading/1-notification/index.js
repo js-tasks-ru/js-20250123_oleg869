a0209bdc85f53c8bf326bf45d59dcfa9eaaed6eb
ArefOleg <serd-075@mail.ru>
2025-02-06 09:18:27 +0300

    база

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,29 @@
 export default class NotificationMessage {
+    constructor(message, {duration, type}){
+        this.message = message || '';
+        this.duration = duration || 0;
+        this.type = type || '';
+        this.element = setElement();
+    }
+
+    setElement(){
+        const element = document.createElement('div');
+        element.innerHTML = this.getTemplate();
+        return element.firstChild;
+    }
+
+    getTemplate(){
+        return `
+            <div class="notification success" style="--value:20s">
+                <div class="timer"></div>
+                <div class="inner-wrapper">
+                <div class="notification-header">success</div>
+                <div class="notification-body">
+                    Hello world
+                </div>
+                </div>
+            </div>
+        `;
+    }
 
 }
