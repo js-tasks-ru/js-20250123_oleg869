4fef6e62ddb1e3b7620c9f6e68c8a080a337badb
ArefOleg <serd-075@mail.ru>
2025-02-07 08:12:29 +0300

    объект начал исчезать

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,41 +1,54 @@
-export default class NotificationMessage {
+//export default class NotificationMessage {
+    class NotificationMessage {
     constructor(message, {duration, type}){
         this.message = message || '';
         this.duration = duration || 0;
         this.type = type || '';
-        this.element = setElement();
+        this.element = this.setElement();
     }
 
     setElement(){
         const element = document.createElement('div');
         element.innerHTML = this.getTemplate();
-        return element.firstChild;
+        return element.firstElementChild ;
     }
-
-    show(div){ // рендер объекта
-
+  
+    show(){ // рендер объекта
+      const existing = document.querySelector('.notification');
+      if (existing) console.log('object exist');
+    
+      document.body.append(this.element);
+  
+      setTimeout(() => {
+        this.remove();
+        console.log(this.duration);
+      }, this.duration);
     }
-
+  
     getTemplate(){
         return `
-            <div class="notification success" style="--value:20s">
+            <div class="notification success" style="--value:${this.duration}s">
                 <div class="timer"></div>
                 <div class="inner-wrapper">
-                <div class="notification-header">success</div>
+                <div class="notification-header">${this.type}</div>
                 <div class="notification-body">
-                    Hello world
+                    ${this.message}
                 </div>
                 </div>
             </div>
         `;
     }
-
+  
     destroy(){
         this.remove();
     }
-
+  
     remove(){
         this.element.remove();
     }
+  
+  }
 
-}
+notificationMessage = new NotificationMessage('Пивко всем за мой счет', 5000);
+document.body.append(notificationMessage.element);
+notificationMessage.show();
