a0c0327ef746c0b2aed6a5b53a7d3eea5134020e
ArefOleg <serd-075@mail.ru>
2025-02-09 09:26:21 +0300

    добавил саб элемент, пока без простановления

--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,43 +1,46 @@
-//export default class SortableTable {
-class SortableTable {
-  constructor(headerConfig = [{ id, title, sotrable, sortType }],
-    data = [{ id, title, price, sales }]) {
-    this.data = data;
-    this.headerConfig = headerConfig;
-    this.element = this.setElement();
-  }
+export default class SortableTable {
+  //class SortableTable {
+    constructor(headerConfig = [{ id, title, sortable, sortType }],
+      data = [{ id, title, price, sales }]) {
+      this.data = data;
+      this.headerConfig = headerConfig;
+      this.element = this.setElement();
+      this.subElements = this.setSubElements();
+    }
+  
+    setElement() {
+      const element = document.createElement('div');
+      element.innerHTML = this.getTemplate();
+      return element.firstElementChild;
+    }
 
-  setElement() {
-    const element = document.createElement('div');
-    element.innerHTML = this.getTemplate();
-    return element.firstElementChild;
-  }
+    setSubElements(){
+      const subElements = {};
 
-  sort(fieldForSort, order) {
-    const sortInfo = this.headerConfig.find(elem => elem.id === fieldForSort);
-    console.log(sortInfo);
-    const sortType = sortInfo.sortType;
-    const sortAsc = (a, b) => {
-      return sortType === "string" ?
-        a[fieldForSort].localeCompare(b[fieldForSort], ['ru', 'en']) :
-        a[fieldForSort] - b[fieldForSort];
-    };
-    const sortDesc = (a, b) => {
-      return sortType === "string" ?
-        b[fieldForSort].localeCompare(a[fieldForSort], ['ru', 'en']) :
-        b[fieldForSort] - a[fieldForSort];
-    };
-    this.data.sort(order === "asc" ? sortAsc : sortDesc);
-    //this.element.querySelector('.sortable-table__body').innerHTML = this.updateSortedProducts();
-  }
-
-  updateSortedProducts() {
-
-  }
-
-  putProductInTable() {
-    const getProductsFromData = this.data.map(({ id, title, price, sales }) =>
-      `
+      return subElements;
+    }
+  
+    sort(fieldForSort, order){
+        const sortInfo = this.headerConfig.find(elem => elem.id === fieldForSort);
+        if(!sortInfo.sortable) return;
+        const sortType = sortInfo.sortType;
+        const sortAsc = (a, b) => {
+            return sortType === "string" ? 
+            a[fieldForSort].localeCompare(b[fieldForSort], 'ru', 'en') :
+            a[fieldForSort] - b[fieldForSort];
+        };
+        const sortDesc = (a, b) => {
+            return sortType === "string" ? 
+             b[fieldForSort].localeCompare(a[fieldForSort], 'ru', 'en') :
+             b[fieldForSort] - a[fieldForSort];
+        };
+        this.data.sort(order === "asc" ? sortAsc : sortDesc);
+        this.element.querySelector('.sortable-table__body').innerHTML = this.putProductInTable();
+    }
+  
+    putProductInTable() {
+      const getProductsFromData = this.data.map(({id, title, price, sales}) =>
+        `
         <a href="/products/${id}" class="sortable-table__row">
           <div class="sortable-table__cell">
             <img class="sortable-table-image" alt="Image" src=""></div>
@@ -48,11 +51,12 @@ class SortableTable {
           <div class="sortable-table__cell">${sales}</div>
         </a>    
         `
-    ).join('');
-  }
-
-  getTemplate() {
-    return `
+      ).join('');
+      return getProductsFromData;
+    }
+  
+    getTemplate() {
+      return `
         <div data-element="productsContainer" class="products-list__container">
           <div class="sortable-table">
   
@@ -86,78 +90,13 @@ class SortableTable {
           </div>
         </div>
       `;
+    }
+  
+    destroy() {
+      this.remove();
+    }
+  
+    remove() {
+      this.element.remove();
+    }
   }
-
-  destroy() {
-    this.remove();
-  }
-
-  remove() {
-    this.element.remove();
-  }
-}
-
-const data = [
-  {
-    'id': 'soska-(pustyshka)-nuk-10729357',
-    'title': 'Соска (пустышка) NUK 10729357',
-    'price': 3,
-    'sales': 14
-  },
-  {
-    'id': 'tv-tyuner-d-color--dc1301hd',
-    'title': 'ТВ тюнер D-COLOR  DC1301HD',
-    'price': 15,
-    'sales': 13
-  },
-  {
-    'id': 'detskiy-velosiped-lexus-trike-racer-trike',
-    'title': 'Детский велосипед Lexus Trike Racer Trike',
-    'price': 53,
-    'sales': 11
-  },
-  {
-    'id': 'soska-(pustyshka)-philips-scf182/12',
-    'title': 'Соска (пустышка) Philips SCF182/12',
-    'price': 9,
-    'sales': 11
-  },
-  {
-    'id': 'powerbank-akkumulyator-hiper-sp20000',
-    'title': 'Powerbank аккумулятор Hiper SP20000',
-    'price': 30,
-    'sales': 11
-  },
-];
-
-
-const header = [
-  {
-    id: 'title',
-    title: 'Name',
-    sortable: true,
-    sortType: 'string'
-  },
-  {
-    id: 'price',
-    title: 'Price',
-    sortable: true,
-    sortType: 'number'
-  },
-  {
-    id: 'sales',
-    title: 'Sales',
-    sortable: true,
-    sortType: 'number'
-  },
-];
-
-sortable = new SortableTable(header, data);
-document.body.append(sortable.element);
-console.log(sortable.data);
-console.log('before sort');
-const field = 'title';
-
-sortable.sort(field, 'desc');
-
-console.log(sortable.data);
