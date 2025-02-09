351a58b679568c67878f3985d73b81739227fe47
ArefOleg <serd-075@mail.ru>
2025-02-09 17:52:23 +0300

    добавил рендер, пока наведение курсора не допилено, инициализация тоже

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -6,7 +6,19 @@ class Tooltip {
   }
   initialize () {
     this.element = document.createElement('div');
+    this.element.className = 'tooltip';
 
+  }
+
+  mouseCursorOver = (event) => {
+    const pointObject = event.target.closest('[data-tooltip]');
+    if(!pointObject) return;
+
+  };
+
+  render(text){
+    this.element.textContent = text;
+    document.body.append(this.element);
   }
 
   getTemplate(){
