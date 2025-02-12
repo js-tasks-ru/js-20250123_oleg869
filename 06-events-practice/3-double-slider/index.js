70b34b326521d39d307d84b5ae2688a9919c8f66
ArefOleg <serd-075@mail.ru>
2025-02-12 16:47:32 +0300

    слайдер база

--- 06-events-practice/3-double-slider/index.js
+++ 06-events-practice/3-double-slider/index.js
@@ -1,3 +1,32 @@
 export default class DoubleSlider {
+    constructor({
+        min,
+        max,
+        formatValue = value => value,
+        selected = {}
+    } = {}) {
+        this.element = null;
+        
+        //this.formatValue = value => formatValue;
+    }
 
+    setElement() {
+
+    }
+
+    getTemplate() {
+        return `
+            <div class="range-slider">
+                <div class="range-slider__inner">
+                    <span class="range-slider__progress">
+                    </span>
+                    <span class="range-slider__thumb-left">
+                    </span>
+                    <span class="range-slider__thumb-right>
+                    </span>
+                <div>
+                <span>${this.presentValue}</span>
+            </div>
+        `;
+    }
 }
