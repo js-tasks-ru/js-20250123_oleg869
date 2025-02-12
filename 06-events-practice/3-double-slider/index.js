81cc6342358fa0da2997d877216e7979b5f87902
ArefOleg <serd-075@mail.ru>
2025-02-12 19:48:11 +0300

    update

--- 06-events-practice/3-double-slider/index.js
+++ 06-events-practice/3-double-slider/index.js
@@ -5,6 +5,8 @@ export default class DoubleSlider {
         formatValue = value => value,
         selected = {}
     } = {}) {
+        this.min = min;
+        this.max = max;
         this.element = null;
         
         //this.formatValue = value => formatValue;
@@ -17,15 +19,16 @@ export default class DoubleSlider {
     getTemplate() {
         return `
             <div class="range-slider">
+                <span data-element="from">${this.presentValue}</span>
                 <div class="range-slider__inner">
-                    <span class="range-slider__progress">
+                    <span class="range-slider__progress" style="left: 30%; right: 30%">
                     </span>
-                    <span class="range-slider__thumb-left">
+                    <span class="range-slider__thumb-left" style="left: 30%">
                     </span>
-                    <span class="range-slider__thumb-right>
+                    <span class="range-slider__thumb-right style="right: 30%>
                     </span>
                 <div>
-                <span>${this.presentValue}</span>
+                <span data-element="to">${this.presentValue}</span>
             </div>
         `;
     }
