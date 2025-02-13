1e2e7d641e03da6049295098759ac9926f3cc054
ArefOleg <serd-075@mail.ru>
2025-02-13 09:09:25 +0300

    получение сразу двух пунктов, в конструкторе объявить не дает

--- 06-events-practice/3-double-slider/index.js
+++ 06-events-practice/3-double-slider/index.js
@@ -7,29 +7,46 @@ export default class DoubleSlider {
     } = {}) {
         this.min = min;
         this.max = max;
-        this.element = null;
+        this.element = this.setElement();
+        this.formatValue = formatValue;
         
-        //this.formatValue = value => formatValue;
     }
 
     setElement() {
-
+        const element = document.createElement('div');
+        element.className = 'range-slider';
+        element.innerHTML = this.getTemplate();
     }
 
     getTemplate() {
-        return `
-            <div class="range-slider">
+        return `            
                 <span data-element="from">${this.presentValue}</span>
                 <div class="range-slider__inner">
                     <span class="range-slider__progress" style="left: 30%; right: 30%">
                     </span>
                     <span class="range-slider__thumb-left" style="left: 30%">
                     </span>
-                    <span class="range-slider__thumb-right style="right: 30%>
+                    <span class="range-slider__thumb-right" style="right: 30%>
                     </span>
                 <div>
-                <span data-element="to">${this.presentValue}</span>
-            </div>
+                <span data-element="to">${this.presentValue}</span>            
         `;
+    }
+
+    createListener(){
+        const {thumbLeft, thumbRight} = this.getThumbs();
+        thumbLeft.addEventListener('pointerdown', this.handlePointerDown);
+        thumbRight.addEventListener('pointerdown', this.handlePointerDown);
+    }
+    destroyListener(){
+        thumbLeft.addEventListener('pointerdown', this.handlePointerDown);
+        thumbRight.addEventListener('pointerdown', this.handlePointerDown);
+    }
+
+    getThumbs() {
+        return {
+            thumbLeft: this.element.querySelector('.range-slider__thumb-left'),
+            thumbRight: this.element.querySelector('.range-slider__thumb-right')
+        }
     }
 }
