9cee55ff2cf13ed79df56c62961bd9dc111f974d
ArefOleg <serd-075@mail.ru>
2025-02-14 09:56:47 +0300

    добавил получение значений ручников

--- 06-events-practice/3-double-slider/index.js
+++ 06-events-practice/3-double-slider/index.js
@@ -1,22 +1,22 @@
 //export default class DoubleSlider {
-    class DoubleSlider {
+class DoubleSlider {
     constructor({
         min,
         max,
-        formatValue = value =>'$' + value,
+        formatValue = value => '$' + value,
         selected = {}
     } = {}) {
         this.min = min;
-        this.max = max;        
+        this.max = max;
         this.formatValue = formatValue;
         this.selected = {
             from: selected.from ?? min,
             to: selected.to ?? max
         };
         this.element = this.setElement();
-        
+        this.thumbElements = this.getThumbValues();
     }
-    
+
     setElement() {
         const element = document.createElement('div');
         element.className = 'range-slider';
@@ -37,6 +37,14 @@
                 </div>
                 <span data-element="to">${this.formatValue(this.selected.to)}</span>            
         `;
+    }
+
+    getThumbValues() {
+        const thumbElements = this.element.querySelectorAll('[data-element]');
+        return {
+            from: thumbElements[0],
+            to: thumbElements[1]
+        };
     }
     /*
     createListener(){
@@ -60,7 +68,8 @@
 const slider = new DoubleSlider({
     min: 0,
     max: 100,
-    selected: {from: 10, to: 80}
+    selected: { from: 10, to: 60 }
 });
 console.log(slider.min, slider.max, slider.selected);
 document.body.append(slider.element);
+console.log(`${slider.thumbElements.from.textContent}  ${slider.thumbElements.to.textContent}`);
