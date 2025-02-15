5422196b3540317c61387964dbcbcc4d3116209b
ArefOleg <serd-075@mail.ru>
2025-02-15 19:11:21 +0300

    почистил комменты, убрал файлы для теста

--- 06-events-practice/3-double-slider/index.js
+++ 06-events-practice/3-double-slider/index.js
@@ -1,18 +1,17 @@
-//export default class DoubleSlider {
-class DoubleSlider {
+export default class DoubleSlider {
     subElements = {};
     constructor({
         min,
         max,
-        formatValue = value => '$' + value,
+        formatValue = value => value,
         selected = {}
     } = {}) {
         this.min = min ?? 100;
         this.max = max ?? 200;
         this.formatValue = formatValue;
         this.selected = {
-            from: selected.from ?? min,
-            to: selected.to ?? max
+            from: selected.from ?? this.min,
+            to: selected.to ?? this.max
         };
         this.element = this.setElement();
         this.selectSubElements();
@@ -21,7 +20,6 @@ class DoubleSlider {
 
     setElement() {
         const element = document.createElement('div');
-        element.className = 'range-slider';
         element.innerHTML = this.getTemplate();
         return element;
     }
@@ -30,53 +28,113 @@ class DoubleSlider {
         const leftProgress = this.getLeftPercent();
         const rightProgress = this.getRightPercent();
 
-        return `            
-                <span data-element="from">${this.formatValue(this.selected.from)}</span>
-                <div class="range-slider__inner">
-                    <span class="thumbProgress" style="left: ${leftProgress}%; right: ${rightProgress}%">
-                    </span>
-                    <span class="thumbLeft" style="left: ${leftProgress}%">
-                    </span>
-                    <span class="thumbRight" style="right: ${rightProgress}%">
-                    </span>
-                </div>
-                <span data-element="to">${this.formatValue(this.selected.to)}</span>            
-        `;
+        return `    
+              <div class="range-slider">        
+              <span data-element="from">${this.formatValue(this.selected.from)}</span>
+              <div data-element="container" class="range-slider__inner">
+                  <span data-element="thumbProgress" class="range-slider__progress" style="left: ${leftProgress}%; right: ${rightProgress}%">
+                  </span>
+                  <span data-element="thumbLeft" class="range-slider__thumb-left" style="left: ${leftProgress}%">
+                  </span>
+                  <span data-element="thumbRight" class="range-slider__thumb-right" style="right: ${rightProgress}%">
+                  </span>
+              </div>
+              <span data-element="to">${this.formatValue(this.selected.to)}</span>
+              </div>            
+      `;
     }
 
-    getLeftPercent(){
+    getLeftPercent() {
         const total = this.max - this.min;
-        const value = this.from - this.min;
-        return Math.round(value / total * 100) ;
+        const value = this.selected.from - this.min;
+        return Math.round(value / total * 100);
     }
 
-    getRightPercent(){
+    getRightPercent() {
         const total = this.max - this.min;
-        const value = this.from - this.min;
-        return Math.round(value / total * 100) ;
+        const value = this.max - this.selected.to;
+        return Math.round(value / total * 100);
     }
 
     selectSubElements() {
-        this.element.querySelectorAll('[data-element]').forEach(elem => {
-            this.subElements[elem.dataset.element] = elem;
+        this.element.querySelectorAll('[data-element]').forEach(element => {
+            this.subElements[element.dataset.element] = element;
         });
     }
 
-    createListener(){
-        this.subElements.thumbLeft.addEventListener('pointerdown', handleThumbLeftPointerdown);
-        this.subElements.thumbRight.addEventListener('pointerdown', handleThumbRightPointerdown);
+    createListener() {
+        this.subElements.thumbLeft.addEventListener('pointerdown', this.handleThubmPointerDown);
+        this.subElements.thumbRight.addEventListener('pointerdown', this.handleThubmPointerDown);
     };
 
-    handleThumbLeftPointerdown = () =>{
-        
+    processPointerMove = (e) => {
+        const { left, width } = this.subElements.container.getBoundingClientRect();
+        const containerLeftX = left;
+        const containerRightX = left + width;
+        const pointerX = e.clientX;
+        const normalizedPointerX = Math.min(containerRightX, Math.max(containerLeftX, pointerX));
+        const percentPointerX = Math.round((normalizedPointerX - containerLeftX) / (containerRightX - containerLeftX) * 100);
+        return this.min + (this.max - this.min) * percentPointerX / 100;
     }
-}
 
-const slider = new DoubleSlider({
-    min: 0,
-    max: 100,
-    selected: { from: 10, to: 60 }
-});
-console.log(slider.min, slider.max, slider.selected);
-document.body.append(slider.element);
-console.log(`${slider.thumbElements.from.textContent}  ${slider.thumbElements.to.textContent}`);
+    handleThubmPointerDown = (e) => {
+        this.activeThumb = e.target.dataset.element;
+
+        document.addEventListener("pointermove", this.handleDocumentPointerMove);
+        document.addEventListener("pointerup", this.handleDocumentPointerUp);
+    }
+
+    handleDocumentPointerMove = (e) => {
+        if (this.activeThumb === "thumbLeft") {
+            this.selected.from = Math.min(this.selected.to, this.processPointerMove(e));
+            this.subElements.from.textContent = this.formatValue(this.selected.from);
+            this.subElements.thumbLeft.style.left = this.getLeftPercent() + '%';
+            this.subElements.thumbProgress.style.left = this.getLeftPercent() + '%';
+        } else {
+            this.selected.to = Math.max(this.selected.from, this.processPointerMove(e));
+            this.subElements.to.textContent = this.formatValue(this.selected.to);
+            this.subElements.thumbRight.style.right = this.getRightPercent() + '%';
+            this.subElements.thumbProgress.style.right = this.getRightPercent() + '%';
+        }
+    }
+
+
+    handleDocumentPointerUp = (e) => {
+        this.activeThumb = null;
+        this.dispatchCustomEvent();
+
+        document.removeEventListener("pointermove", this.handleDocumentPointerMove);
+        document.removeEventListener("pointerup", this.handleDocumentPointerUp);
+    }
+
+    dispatchCustomEvent() {
+        const event = new CustomEvent("range-select", {
+            detail: {
+                from: this.selected.from,
+                to: this.selected.to
+            },
+        });
+        this.element.dispatchEvent(event);
+    }
+
+    remove() {
+        this.element.remove();
+    }
+
+    destroyEventListeners() {
+        this.subElements.thumbLeft.removeEventListener(
+            "pointerdown",
+            this.handleThumbPointerdown
+        );
+        this.subElements.thumbRight.removeEventListener(
+            "pointerdown",
+            this.handleThumbPointerdown
+        );
+    }
+
+    destroy() {
+        this.destroyEventListeners();
+        this.element.remove();
+    }
+
+}
