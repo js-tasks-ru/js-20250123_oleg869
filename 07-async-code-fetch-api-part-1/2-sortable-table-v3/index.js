22b531cbe5282d1aedcb1d87b81beb29b2bc396b
ArefOleg <serd-075@mail.ru>
2025-02-18 17:01:33 +0300

    прогружает доп данные

--- 07-async-code-fetch-api-part-1/2-sortable-table-v3/index.js
+++ 07-async-code-fetch-api-part-1/2-sortable-table-v3/index.js
@@ -2,13 +2,15 @@ import fetchJson from './utils/fetch-json.js';
 import SortableTableV2 from '../../06-events-practice/1-sortable-table-v2/index.js';
 const BACKEND_URL = 'https://course-js.javascript.ru';
 export default class SortableTable extends SortableTableV2 {
-  constructor(headersConfig, { sorted = {}, url, isSortLocally = false, pageSize = 30, scrolledRecords = 10} = {}) {
+  constructor(headersConfig, { sorted = {}, url, isSortLocally = false, pageSize = 30, scrolledRecords = 10 } = {}) {
     super(headersConfig);
     this.isSortLocally = isSortLocally;
     this.url = new URL(url, BACKEND_URL);
     this.sorted.id = sorted.id;
     this.sorted.order = sorted.order;
     this.pageSize = pageSize;
+    this.startPosition = 0;
+    this.hasMoreData = true;
     this.render(sorted.id, sorted.order);
     this.handleScroll;
     this.scrolledRecords = scrolledRecords;
@@ -21,9 +23,8 @@ export default class SortableTable extends SortableTableV2 {
     console.log(`scrollY=${scrollY} innerHeight=${innerHeight} scrollHeight=${scrollHeight}`);
     console.log(`scrollY + innerHeight = ${scrollY + innerHeight}`);
     console.log(`scrollHeight - this.scrolledRecords = ${scrollHeight - this.scrolledRecords}`);
-    if(scrollY + innerHeight >= scrollHeight){
-      console.log('limit achieved');
-      //await this.loadNextData();
+    if (scrollY + innerHeight >= scrollHeight) {
+      await this.loadNextStack();
     }
 
 
@@ -35,7 +36,7 @@ export default class SortableTable extends SortableTableV2 {
       reUsedURL.searchParams.set('_embed', 'subcategory.category');
       reUsedURL.searchParams.set('_sort', sortField);
       reUsedURL.searchParams.set('_order', sortOrder);
-      reUsedURL.searchParams.set('_start', 0);
+      reUsedURL.searchParams.set('_start', this.startPosition);
       reUsedURL.searchParams.set('_end', this.pageSize);
 
       const response = await fetch(reUsedURL.href);
@@ -53,7 +54,26 @@ export default class SortableTable extends SortableTableV2 {
 
   }
 
-  createScrollListener(){
+  async loadNextStack() {
+    this.startPosition = this.pageSize + 1;
+    this.pageSize = this.pageSize * 2;
+    try {
+      const newData = await this.loadData(this.sorted.id, this.sorted.order);
+      if (newData.length === 0) {
+        this.hasMoreData = false;
+        return;
+      }
+      this.data = [...this.data, ...newData];
+      await this.render();
+    }
+
+    catch (e) {
+      console.error('Error loading more data', e);
+    }
+
+  }
+
+  createScrollListener() {
     window.addEventListener('scroll', this.handleWindowScroll);
   }
 
@@ -67,7 +87,7 @@ export default class SortableTable extends SortableTableV2 {
     this.subElements.body.innerHTML = this.getTableBody();
   }
 
-  destroy(){
+  destroy() {
     super.destroy();
     window.removeEventListener('scroll', this.handleWindowScroll);
   }
