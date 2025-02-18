c045b81d0172257bc2cb58c113257f6d5f5dadac
ArefOleg <serd-075@mail.ru>
2025-02-18 08:21:33 +0300

    7-2 без инфинити скрола

--- 07-async-code-fetch-api-part-1/2-sortable-table-v3/index.js
+++ 07-async-code-fetch-api-part-1/2-sortable-table-v3/index.js
@@ -2,12 +2,13 @@ import fetchJson from './utils/fetch-json.js';
 import SortableTableV2 from '../../06-events-practice/1-sortable-table-v2/index.js';
 const BACKEND_URL = 'https://course-js.javascript.ru';
 export default class SortableTable extends SortableTableV2 {
-  constructor(headersConfig, { sorted = {}, url, isSortLocally = false } = {}) {
+  constructor(headersConfig, { sorted = {}, url, isSortLocally = false, pageSize = 30} = {}) {
     super(headersConfig);
     this.isSortLocally = isSortLocally;
     this.url = new URL(url, BACKEND_URL);
     this.sorted.id = sorted.id;
     this.sorted.order = sorted.order;
+    this.pageSize = pageSize;
     this.render(sorted.id, sorted.order);
   }
 
@@ -18,7 +19,7 @@ export default class SortableTable extends SortableTableV2 {
       reUsedURL.searchParams.set('_sort', sortField);
       reUsedURL.searchParams.set('_order', sortOrder);
       reUsedURL.searchParams.set('_start', 0);
-      reUsedURL.searchParams.set('_end', 30);
+      reUsedURL.searchParams.set('_end', this.pageSize);
 
       const response = await fetch(reUsedURL.href);
 
