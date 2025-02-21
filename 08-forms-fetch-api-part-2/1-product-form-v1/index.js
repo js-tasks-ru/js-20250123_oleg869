d0ea470754796264f02f79600883bbef2c98b424
ArefOleg <serd-075@mail.ru>
2025-02-21 08:25:36 +0300

    8-1 база

--- 08-forms-fetch-api-part-2/1-product-form-v1/index.js
+++ 08-forms-fetch-api-part-2/1-product-form-v1/index.js
@@ -5,11 +5,52 @@ const IMGUR_CLIENT_ID = '28aaa2e823b03b1';
 const BACKEND_URL = 'https://course-js.javascript.ru';
 
 export default class ProductForm {
-  constructor (productId) {
+  element;
+  subElements = {};
+
+  constructor(productId) {
     this.productId = productId;
+
+    this.createElement();
+    this.render();
   }
 
-  async render () {
+  createElement() {
+    const element = document.createElement('div');
+    element.innerHTML = this.getTempalte();
+    this.element = element.firstElementChild;
+  }
 
+  getSubElements() {
+    return this.element.querySelectorAll('[data-element]').forEach(
+      element => {
+        this.subElements[element.dataset.element] = element;
+      });
+  }
+
+  async render() {
+    this.createElement();
+    this.getSubElements();
+
+    return this.element;
+  }
+
+  getTempalte() {
+    return `
+      <div class="product-form">
+         <form data-element="productForm" class="form-grid">
+          <div class="form-group form-group__half_left">
+            <fieldset>
+              <label class="form-label">Название товара</label>
+              <input required="" type="text" name="title" class="form-control" placeholder="Название товара">
+            </fieldset>
+          </div>
+          <div class="form-group form-group__wide">
+              <label class="form-label">Описание</label>
+              <textarea required="" class="form-control" name="description" data-element="productDescription" placeholder="Описание товара"></textarea>
+          </div>
+         </form>
+      </div>
+    `
   }
 }
