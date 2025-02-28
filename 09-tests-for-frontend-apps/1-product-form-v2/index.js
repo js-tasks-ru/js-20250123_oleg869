add27c881cb5909d6dba2caf76e4facfdd743cd3
ArefOleg <serd-075@mail.ru>
2025-02-28 12:02:52 +0300

    начать надо с 2

--- 09-tests-for-frontend-apps/1-product-form-v2/index.js
+++ 09-tests-for-frontend-apps/1-product-form-v2/index.js
@@ -1,16 +1,29 @@
-import SortableList from '../../2-sortable-list/src/index.js';
+//import SortableList from '../../2-sortable-list/src/index.js';
 import escapeHtml from './utils/escape-html.js';
 import fetchJson from './utils/fetch-json.js';
+import ProductFormV1 from '../../08-forms-fetch-api-part-2/1-product-form-v1/index.js';
 
 const IMGUR_CLIENT_ID = '28aaa2e823b03b1';
 const BACKEND_URL = 'https://course-js.javascript.ru';
 
-export default class ProductForm {
+export default class ProductForm extends ProductFormV1 {
   constructor (productId) {
-    this.productId = productId;
+    super(productId);
   }
 
   async render () {
-
+    await super.render();
+    this.setSortableList();
+    return this.element;
   }
+
+  setSortableList(){
+    const { imageListContainer } = this.subElements;
+
+    const SortableList
+  }
+
+
+
+
 }
